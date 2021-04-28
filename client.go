bd7e2272c1473d0fd2592edf91eacd93981ca8be
Ian Childress <ianchildress@users.noreply.github.com>
2021-04-27 18:36:59 -0700

    added ServerTime method

--- client.go
+++ client.go
@@ -1,8 +1,14 @@
 package kraken
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"time"
 )
+
+const baseURL = "https://api.kraken.com"
 
 type Client struct {
 	key    string
@@ -15,4 +21,50 @@ func NewClient(key, secret string) Client {
 
 func nonce() int64 {
 	return time.Now().Unix()
+}
+
+const serverTimePath = "/0/public/Time"
+
+type ServerTimeResponse struct {
+	Error  []error
+	Result ServerTimeResult
+}
+
+type ServerTimeResult struct {
+	UnixTime int64
+	RFC1123  time.Time
+}
+
+func (c Client) ServerTime() (ServerTimeResponse, error) {
+	var out ServerTimeResponse
+
+	req, err := http.NewRequest("GET", path(baseURL, serverTimePath), nil)
+	if err != nil {
+		return out, err
+	}
+
+	req.Header.Set("Cache-Control", "no-cache")
+
+	client := &http.Client{Timeout: time.Second * 10}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return out, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return out, err
+	}
+
+	if err := json.Unmarshal(body, &out); err != nil {
+		return out, err
+	}
+
+	return out, nil
+}
+
+func path(base, suffix string) string {
+	return fmt.Sprintf("%s%s", base, suffix)
 }
